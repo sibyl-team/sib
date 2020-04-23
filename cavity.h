37c2e0e312885beb5a6a9654978170e3fe033f20
Alfredo Braunstein <abraunst+github@gmail.com>
2020-04-23 08:32:57 +0200

    faster, non-allocating cavity

--- cavity.h
+++ cavity.h
@@ -12,7 +12,7 @@ public:
 	template<class container_t, typename C>
 	Cavity(container_t const & c, T const & init, C const & convolute)
 	{
-		  initialize(c.begin(), c.end(), init, convolute);
+		initialize(c.begin(), c.end(), init, convolute);
 	}
 
 	template<class iterator_t, typename C>
@@ -23,6 +23,7 @@ public:
 	template<class iterator_t, typename C>
 	void initialize(iterator_t const & beg, iterator_t const & end, T const & init, C const & convolute)
 	{
+		Cavity & me = *this;
 		std::vector<T>::resize(distance(beg, end), init);
 		int const n = std::vector<T>::size();
 		if (n == 0) {
@@ -31,29 +32,28 @@ public:
 		}
 
 		if (n == 1) {
-			std::vector<T>::operator[](0) = init;
+			me[0] = init;
 			full_ = *beg;
 			return;
 		}
 
-		std::vector<T> right(n);
 		iterator_t beg2 = beg, end2 = end;
 		--end2;
-		right[n - 1] = *end2;
+		me[n - 1] = *end2;
 		//--end2;
 		for (int i = n - 2; i >= 0; --i)
-			right[i] = convolute(*(--end2), right[i + 1]);
-		full_ = right[0];
+			me[i] = convolute(*(--end2), me[i + 1]);
+		full_ = me[0];
 
-		std::vector<T>::operator[](0) = right[1];
+		me[0] = me[1];
 		T left = *beg2;
 		for (int i = 1; i < n - 1; ++i) {
-			std::vector<T>::operator[](i) = convolute(left, right[i + 1]);
+			me[i] = convolute(left, me[i + 1]);
 			//std::cout << left.size() << " " << (beg2 + 1)->size() << "  " << i << " " << n << std::endl;
 			//assert(left.size() == (beg2 + 1)->size());
 			left = convolute(left, *(++beg2));
 		}
-		std::vector<T>::operator[](n - 1) = left;
+		me[n - 1] = left;
 	}
 
 	T const & full() const { return full_; }
