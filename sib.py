0a16565252bf436ff79318a48daff25cc256a05c
ocadni <ocadni@gmail.com>
2020-04-24 16:41:42 +0200

    add flush to python script

--- sib.py
+++ sib.py
@@ -1,7 +1,7 @@
 from _sib import *
 
 
-def iterate(f, maxit=100, tol=1e-3, callback=(lambda t, err, f: print(t, err))):
+def iterate(f, maxit=100, tol=1e-3, callback=(lambda t, err, f: print(t, err, flush=True))):
     for t in range(maxit):
         err = f.update()
         if callback(t, err, f) == False:
