9974c4f0a3ba235627829663dcaa709b19242530
ocadni <ocadni@gmail.com>
2020-04-24 16:29:12 +0200

    fix err

--- sib.py
+++ sib.py
@@ -4,7 +4,7 @@ from _sib import *
 def iterate(f, maxit=100, tol=1e-3, callback=(lambda t, err, f: print(t, err))):
     for t in range(maxit):
         err = f.update()
-        if callback(t, err, f) == False
+        if callback(t, err, f) == False:
             break;
         if err < tol:
             break;
