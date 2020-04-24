70d188137c04d22d4b8844f88e6f5fb72829673a
Alfredo Braunstein <abraunst+github@gmail.com>
2020-04-24 16:56:10 +0200

    add times method

--- pysib.cpp
+++ pysib.cpp
@@ -24,19 +24,35 @@ string showfg(FactorGraph const & f) {
     return "sib.FactorGraph with " + to_string(f.nodes.size()) + " nodes";
 }
 
+map<int, vector<int> > get_times(FactorGraph const & f) {
+    map<int, vector<int> > times;
+    for (int i = 0; i < int(f.nodes.size()); ++i) {
+        (times[f.nodes[i].index] = f.nodes[i].times).pop_back();
+    }
+    return times;
+}
+
 PYBIND11_MODULE(_sib, m) {
     py::class_<FactorGraph>(m, "FactorGraph")
-        .def(py::init<vector<tuple<int,int,int,real_t> >, vector<tuple<int,int,int> >, Params const &>(), py::arg("contacts"), py::arg("observations"), py::arg("params"))
+        .def(py::init<vector<tuple<int,int,int,real_t> >,
+                vector<tuple<int,int,int> >,
+                Params const &>(),
+                py::arg("contacts"),
+                py::arg("observations"),
+                py::arg("params"))
         .def("update", &FactorGraph::iteration)
         .def("bt", &FactorGraph::get_tbeliefs)
         .def("bg", &FactorGraph::get_gbeliefs)
         .def("reset", &FactorGraph::init)
+        .def("times", &get_times)
         .def("__repr__", &showfg)
         .def_readwrite("params", &FactorGraph::params);
 
 
     py::class_<Params>(m, "Params")
-        .def(py::init<real_t, real_t>(), py::arg("mu") = 0.01, py::arg("pseed") = 0.01)
+        .def(py::init<real_t, real_t>(),
+                py::arg("mu") = 0.01,
+                py::arg("pseed") = 0.01)
         .def_readwrite("mu", &Params::mu)
         .def_readwrite("pseed", &Params::pseed)
         .def("__repr__", &showparams);
