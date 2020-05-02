c02a5ba41c1b73d87b46484525bca2fee1ee4665
Fabio Mazza <fab4mazz@gmail.com>
2020-05-02 14:50:13 +0200

    Add license prefix

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
+# Copyright 2020 Sybil team
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# 
 # This file is part of sibilla : inference in epidemics with Belief Propagation
 # Author: Fabio Mazza
 
@@ -122,12 +136,14 @@ class SibillaTest(unittest.TestCase):
     def test_beliefs(self):
         print("\n--- Testing beliefs ---")
         new_all_beliefs = [self.calc_beliefs(i) for i in range(self.n_inst)]
+        v = 1e-60
         for i in range(self.n_inst):
             for n in range(self.num_nodes):
                 #with self.subTest(inst=i,node=n):
                 msg_fail = "Test on inst {} for node {} failed".format(i,n)
-                self.assertEqual(np.all(new_all_beliefs[i][n] == self.loaded_beliefs[i][n]),True,msg_fail)
-
+                v = max(v,np.max(new_all_beliefs[i][n]-self.loaded_beliefs[i][n]))
+                self.assertEqual(np.all(new_all_beliefs[i][n] - self.loaded_beliefs[i][n] < 1e-12),True,msg_fail)
+        print(v)
 
 if __name__ == '__main__':
     unittest.main(failfast=False)
