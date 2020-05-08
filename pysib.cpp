f285dfc4cab6e65b41f1b0a619ed66dd6a367499
Alfredo Braunstein <abraunst+github@gmail.com>
2020-05-08 17:50:49 +0200

    guard against wrong t in get_marginal

--- pysib.cpp
+++ pysib.cpp
@@ -79,7 +79,10 @@ get_marginal(Node const & n)
 
 tuple<real_t, real_t, real_t> get_marginal_t(Node const & n, int t)
 {
-        return get_marginal(n)[t];
+        if (t < 0 || t >= n.bt.size() - 2)
+            throw py::key_error("Time out of range");
+        else
+            return get_marginal(n)[t];
 }
 
 
