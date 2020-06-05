5fd2db048904a8a675acd55fb20abade0b885e63
Alfredo Braunstein <abraunst+github@gmail.com>
2020-06-05 12:55:54 +0200

    missing header modification

--- drop.h
+++ drop.h
@@ -2,3 +2,6 @@
 
 std::tuple<int, real_t, real_t>
 drop_sc(FactorGraph & fg, int t, int maxit_bp, real_t tol_bp, real_t damping_bp, int maxit_sc, real_t tol_sc, real_t damping_sc);
+
+
+void drop_time(FactorGraph & fg, int t);
